cc4a577030ac80925ddbadd53abde0b609c3e2e5
Avanish Kulkarni <bigfatnotebook@gmail.com>
2024-10-08 22:39:13 -0400

    file reading logic

--- lexer.c
+++ lexer.c
@@ -3,5 +3,27 @@
 
 int main(int argc, char **argv)
 {
+    FILE *file;
+    char ch;
+
+    if (argc == 1)
+    {
+        perror("Missing filename command-line argument!");
+        return 1;
+    }
+
+    file = fopen(argv[1], 'r');
+
+    if (file == NULL)
+    {
+        perror("Error opening file!");
+        return 1;
+    }
+
+    while ((ch = fgetc(file)) != EOF)
+    {
+        putchar(ch);
+    }
+
     return 0;
 }
